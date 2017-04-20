285654739d777f35b59b0bf67726b04d7598597e
王玮 <316817495@qq.com>
2017-04-20 16:01:37 +0800

    弹框样式1，暂定1

--- my-project/src/components/folderPopup.vue
+++ my-project/src/components/folderPopup.vue
@@ -7,7 +7,7 @@
             
         </div>
         <div class="titleText">
-            <span>folderPopup</span>
+            <span><i class="icon-screen-desktop"></i> 我的电脑</span>
 
             <div class="btnGroup">
                    <a class="controlBtn yellowBtn" @click="">
@@ -16,15 +16,14 @@
                   <a class="controlBtn redCloseBtn" @click="iconCloseBtn">
                       <i class="icon-close"></i>
                   </a>
-                 
             </div>
             
 
         </div>
     </el-col>
    
-    <el-col :span="24">
-          <div class="folderPopup_cont">
+    <el-col :span="24" class="folderPopup_cont">
+          <div class="folderPopup_contBg">
           </div>
      </el-col>
 
@@ -48,30 +47,35 @@ export default {
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
     .folderPopup{
       top:75px;
-      width: 500px;
-      height: 500px;
+      width: 75%;
+      /*height: 500px;*/
+      bottom:35px;
       border-radius: 10px;
+      overflow: hidden;
       /*background-color: #fff;*/
       position: absolute;
-      background: #eee;
-      background-color: rgba(190,190,190,0.5);
+      border:3px solid rgba(190,190,190,0.7);
+     /* background: #eee;
+      background-color: rgba(190,190,190,0.5);*/
       box-shadow: 0 0 10px rgba(0,0,0,0.2);
+      background-color: #fff;
     }
     .folderPopup .titleText{
       position: relative;
-      margin:5px 5px;
+      padding:10px 10px;
+      background-color: rgba(190,190,190,0.7);
     }
-   .folderPopup .folderPopup_cont{
+    
+  /* .folderPopup .folderPopup_cont{
         background-color: #fff;
         width: 98%;
         height: 93%;
         margin:5px;
         border-radius: 10px;
-   }
+   }*/
    .controlBtn{
       margin-left:5px;
       float:left;
@@ -82,10 +86,10 @@ export default {
         color: #aaa;
       border: 1px solid #bbb;
       border-radius: 10px;
-      height: 18px;
-      width: 18px;
-      font-size: 1em;
-      line-height: 18px;
+      height: 20px;
+      width: 20px;
+      font-size: 1.2em;
+      line-height: 20px;
       text-align: center;
       filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#eeeeee', endColorstr='#bbbbbb');
       background: #bbb;
@@ -123,7 +127,14 @@ export default {
 
    .btnGroup{
       position: absolute;
-      top: 0;
-      right: 0;
+      top: 5px;
+      right:  5px;
+   }
+   .folderPopup .folderPopup_cont{
+      position:relative;
+    
+   }
+   .folderPopup .folderPopup_contBg{
+      
    }
 </style>
