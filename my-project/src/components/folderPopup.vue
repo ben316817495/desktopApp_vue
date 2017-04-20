6d00932cb59c608bff059896cd4e0def54063602
王玮 <316817495@qq.com>
2017-04-20 12:13:03 +0800

    页面完善

--- my-project/src/components/folderPopup.vue
+++ my-project/src/components/folderPopup.vue
@@ -3,7 +3,15 @@
     <el-row>
      
      <el-col :span="24">
-      <div class="titleText">folderPopup</div>
+        <div class="">
+            
+        </div>
+        <div class="titleText">
+            <span>folderPopup</span>
+            <a class="controlBtn redCloseBtn">
+                  
+            </a>
+        </div>
     </el-col>
    
     <el-col :span="24">
@@ -48,5 +56,40 @@ export default {
         height: 93%;
         margin:5px;
         border-radius: 10px;
+   }
+   .controlBtn{
+      display: block;
+      position: absolute;
+      text-decoration: none;
+      outline: none;
+      cursor: pointer;
+        color: #aaa;
+      top: 4px;
+      border: 1px solid #bbb;
+      border-radius: 10px;
+      height: 18px;
+      width: 18px;
+      font-size: 1em;
+      line-height: 18px;
+      text-align: center;
+          filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#eeeeee', endColorstr='#bbbbbb');
+    background: #bbb;
+    background-image: -webkit-linear-gradient(top,#eee,#bbb);
+    background-image: -moz-linear-gradient(top,#eee,#bbb);
+    background-image: -o-linear-gradient(top,#eee,#bbb);
+    background-image: -ms-linear-gradient(top,#eee,#bbb);
+    background-image: linear-gradient(top,#eee,#bbb);
+    -pie-background: linear-gradient(to top,#eee,#bbb);
+   }
+   .redCloseBtn:hover,.redCloseBtn:active{
+    background-color: #de2f0a;
+     filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#eeeeee', endColorstr='#de2f0a');
+    background: #de2f0a;
+    background-image: -webkit-linear-gradient(top,#f7d8d8,#de2f0a);
+    background-image: -moz-linear-gradient(top,#f7d8d8,#de2f0a);
+    background-image: -o-linear-gradient(top,#f7d8d8,#de2f0a);
+    background-image: -ms-linear-gradient(top,#f7d8d8,#de2f0a);
+    background-image: linear-gradient(top,#f7d8d8,#de2f0a);
+    -pie-background: linear-gradient(to top,#f7d8d8,#de2f0a);
    }
 </style>
--- my-project/src/main.js
+++ my-project/src/main.js
@@ -5,6 +5,8 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-default/index.css'
+
+
 Vue.use(ElementUI)
 
 Vue.config.productionTip = false
