c7b672b96160dbb33fd7f1e32bd567efbf0460c2
王玮 <316817495@qq.com>
2017-05-04 18:07:33 +0800

    自豪的加入less

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -6,25 +6,27 @@
 </template>
 
 <script>
+
+ import '@/assets/css/less-css/style.css';
 export default {
   name: 'app'
 }
 </script>
 
 <style>
-*{font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;}
+/**{font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","微软雅黑",Arial,sans-serif;}*/
 * {
     -webkit-box-sizing: border-box;
     -moz-box-sizing: border-box;
     box-sizing: border-box;
 }
-html,body{
+/*html,body{
    height: 100%;
    width: 100%;
     margin:0;
     padding: 0;
     overflow-x: hidden;
-}
+}*/
 #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
@@ -33,5 +35,8 @@ html,body{
   }
   .heightFull{
     height:100%;
+  }
+  .height90{
+
   }
 </style>
