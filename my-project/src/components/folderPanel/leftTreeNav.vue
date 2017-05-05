79bff70705be15ef02724e9295b3980ec8256bef
王玮 <316817495@qq.com>
2017-05-05 16:28:52 +0800

    加入JSX插件，修改树形控件样式

--- my-project/src/components/folderPanel/leftTreeNav.vue
+++ my-project/src/components/folderPanel/leftTreeNav.vue
@@ -1,8 +1,75 @@
 <template>
 	<div class="leftTreeNav">
 		<div class="treeExternal">
-			<span v-for="n in 100">leftTreeNav</span>
+			<el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
 		</div>
 	</div>
 </template>
 
+<script>
+  export default {
+    data() {
+      return {
+        data: [{
+          label: '一级 1',
+          children: [{
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+          children: [{
+            label: '二级 3-1',
+            children: [{
+              label: '三级 3-1-1'
+            }]
+          }, {
+            label: '二级 3-2',
+            children: [{
+              label: '三级 3-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
+      };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      }
+    }
+  };
+</script>
