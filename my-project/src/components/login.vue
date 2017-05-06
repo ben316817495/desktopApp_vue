3740e86df8fa23e28350fc0282460328a1163eb1
王玮 <316817495@qq.com>
2017-05-06 15:55:18 +0800

    登陆页面优化

--- my-project/src/components/login.vue
+++ my-project/src/components/login.vue
@@ -5,6 +5,32 @@
 			<div class="boxHeader">
 					<div class="boxHeaderImg"></div>
 			</div>
+			<div class="boxBody">
+				<el-form ref="form" :model="form" label-width="80px">
+						<el-form-item label="用户名：">
+					    	<el-input v-model="form.name"></el-input>
+					 	</el-form-item>
+					 	<el-form-item label="密码：">
+					    	<el-input v-model="form.pwd"></el-input>
+					 	</el-form-item>
+						<el-form-item >
+							<el-row>
+								<el-col :span="12">
+									 <el-checkbox-group v-model="form.type">
+										<el-checkbox  label="记住密码"></el-checkbox>
+									 </el-checkbox-group>
+								</el-col>
+								<el-col :span="12">
+									
+								</el-col>
+							</el-row>
+						</el-form-item>
+						<el-form-item>
+						    <el-button type="primary" @click="onSubmit">立即登陆</el-button>
+						    <el-button>忘记密码</el-button>
+						</el-form-item>
+				</el-form>
+			</div>
 		</div>
 	</div>
 </template>
@@ -13,5 +39,15 @@
 	export default {
 	  name: 'loginPage',
 	  components: {},
+	  data() {
+      	return {
+	      	form: {
+	      			name:'',
+	      			pwd:'',
+	      			delivery: false,
+	      			type: [],
+	      		}
+      		}
+  		},//data
 	}
 </script>
