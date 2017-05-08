b50c6a7b128157b6071bea454890375a2dfcdc01
ben <316817495@qq.com>
2017-05-08 22:01:33 +0800

    cmd模块构建

--- my-project/src/components/applicationOfCMD/cmdApplication.vue
+++ my-project/src/components/applicationOfCMD/cmdApplication.vue
@@ -1,5 +1,41 @@
 <template>
 	<div class="cmdApplication">
-		1111111111
+		<el-row class="heightFull">
+     
+		     <el-col :span="24">
+		        <div class="">
+		            
+		        </div>
+		        <div class="titleText">
+		            <span><i class="icon-screen-desktop"></i> CMD 管理员:Bone7</span>
+
+		            <div class="btnGroup">
+		                   <a class="controlBtn yellowBtn" @click="">
+		                      <i class="icon-minus"></i>
+		                  </a>
+		                  <a class="controlBtn redCloseBtn" @click="iconCloseBtn">
+		                      <i class="icon-close"></i>
+		                  </a>
+		            </div>
+
+		        </div>
+		    </el-col>
+		</el-row>
 	</div>
 </template>
+
+<script>
+export default {
+  components: {},
+  data () {
+    return {
+
+    }
+  },//data()
+  methods: {
+    iconCloseBtn(){
+      this.$emit('btnListenEvent','cmdClose');
+    },//iconCloseBtn()
+  },//methods
+}
+</script>
