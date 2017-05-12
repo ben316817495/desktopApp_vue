f00c2258c656afd6d858f47e5e1c021e81aa94c0
ben <316817495@qq.com>
2017-05-12 21:32:32 +0800

    音乐盒晚上修改

--- my-project/src/components/musicPanel/musicPlayPanel.vue
+++ my-project/src/components/musicPanel/musicPlayPanel.vue
@@ -10,16 +10,32 @@
 		</div>
 		<div class="musicControl">
 			<el-row>
-				<el-col :span="3" class="timeText-left">
-					00:00
+				<el-col :span="2" class="timeText-left">
+					<a><i class="icon-control-rewind"></i></a>
 				</el-col>
-				<el-col :span="18" class="timeLine">
+				<el-col :span="20" class="timeLine">
 					<el-slider v-model="value1"></el-slider>
 				</el-col>
-				<el-col :span="3" class="timeText-right">
-					24:00
+				<el-col :span="2" class="timeText-right">
+					<a><i class="icon-control-forward"></i></a>
+				</el-col>
+				<el-col :span="3" class="textTimeC">00:00/04:00</el-col>
+				<el-col :span="15" class="musicControl_btn">
+					<ul>
+						<li><i class="icon-control-start"></i></li>
+						<li><i class="icon-control-play"></i></li>
+						<li><i class="icon-control-pause"></i></li>
+						<li><i class="icon-control-end"></i></li>
+						<li><i class="icon-heart"></i></li>
+						<li><i class="icon-share"></i></li>
+					</ul>
+				</el-col>
+				<el-col :span="3" class="mus_vcon">
+					<p><i class="icon-volume-2"></i></p> <el-slider v-model="value1" class="vconStyle"></el-slider>
 				</el-col>
 			</el-row>
 		</div>
+
+		
 	</div>
 </template>
