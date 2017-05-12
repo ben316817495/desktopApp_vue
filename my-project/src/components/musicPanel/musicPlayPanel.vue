ca5ded8bc07f7089de5bd7180e41afe75db6ee57
王玮 <316817495@qq.com>
2017-05-12 18:05:27 +0800

    音乐盒

--- my-project/src/components/musicPanel/musicPlayPanel.vue
+++ my-project/src/components/musicPanel/musicPlayPanel.vue
@@ -1,5 +1,16 @@
 <template>
 	<div class="musicPlayPanel">
-		musicPlayPanel
+		<div class="playAnimation"></div>
+		<div class="playMusicTitle">
+			你经历过绝望吗？
+		</div>
+		<div class="musicMess">
+			<i class="icon-playlist"></i> 专辑：bone7大神显神威 
+			<i class="icon-music-tone-alt"></i> 歌手：bone7
+		</div>
+		<div class="musicControl">
+		<el-row></el-row>
+			<el-slider v-model="value1"></el-slider>
+		</div>
 	</div>
 </template>
