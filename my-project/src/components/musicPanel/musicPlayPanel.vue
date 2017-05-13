4a56d7b71abec354343338257633546aa0632e97
王玮 <316817495@qq.com>
2017-05-13 15:53:38 +0800

    音乐盒

--- my-project/src/components/musicPanel/musicPlayPanel.vue
+++ my-project/src/components/musicPanel/musicPlayPanel.vue
@@ -1,6 +1,13 @@
 <template>
 	<div class="musicPlayPanel">
-		<div class="playAnimation"></div>
+		<div class="loopMusicBtn"  @click="loopPlayBtn">
+			<i v-if="playLoop" class="icon-refresh"></i>
+			<i v-else class="icon-reload"></i>
+		</div>
+		<div class="likeMusicBtn"><i class="icon-heart"></i></div>
+		<div class="shareMusicBtn"><i class="icon-share"></i></div>
+
+		<div class="playAnimation" v-bind:class="{'addPlayAnimation':playClass}"></div>
 		<div class="playMusicTitle">
 			你经历过绝望吗？
 		</div>
@@ -14,7 +21,7 @@
 					<a><i class="icon-control-rewind"></i></a>
 				</el-col>
 				<el-col :span="20" class="timeLine">
-					<el-slider v-model="value1"></el-slider>
+					<el-slider v-model="value1" format-tooltip="formatTooltip"></el-slider>
 				</el-col>
 				<el-col :span="2" class="timeText-right">
 					<a><i class="icon-control-forward"></i></a>
@@ -23,15 +30,17 @@
 				<el-col :span="15" class="musicControl_btn">
 					<ul>
 						<li><i class="icon-control-start"></i></li>
-						<li><i class="icon-control-play"></i></li>
-						<li><i class="icon-control-pause"></i></li>
+						<li  @click="onPlayAnimation">
+							<i class="icon-control-play"></i>
+						</li>
+						<li  @click="stopPlayAnimation"><i class="icon-control-pause"></i></li>
 						<li><i class="icon-control-end"></i></li>
-						<li><i class="icon-heart"></i></li>
-						<li><i class="icon-share"></i></li>
+						<!-- <li><i class="icon-heart"></i></li>
+						<li><i class="icon-share"></i></li> -->
 					</ul>
 				</el-col>
 				<el-col :span="3" class="mus_vcon">
-					<p><i class="icon-volume-2"></i></p> <el-slider v-model="value1" class="vconStyle"></el-slider>
+					<p><i class="icon-volume-2"></i></p> <el-slider v-model="value2" class="vconStyle"></el-slider>
 				</el-col>
 			</el-row>
 		</div>
@@ -39,3 +48,31 @@
 		
 	</div>
 </template>
+
+
+<script>
+  export default {
+    data() {
+      return {
+      	playLoop:false,
+      	playClass:false,
+        value1: 0,
+        value2: 50,
+      }
+    },
+    methods: {
+      formatTooltip(val) {
+        return val / 100;
+      },//formatTooltip
+      loopPlayBtn(){
+      	this.playLoop = !this.playLoop;
+      },//loopPlayBtn()
+      onPlayAnimation(){
+      		this.playClass = !this.playClass;
+      },//onPlayAnimation
+      stopPlayAnimation(){
+      	this.playClass= false;
+      },//stopPlayAnimation
+    }
+  }
+</script>
